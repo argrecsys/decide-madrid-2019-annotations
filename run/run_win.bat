e839a897e2a4362cd84135355f72256b42aaf4f0
Andres Segura-Tinoco <seguraandres7@gmail.com>
2022-03-15 17:26:47 +0100

    Create run folder
    The prodigy run folder has been created with the env variables to be created and the anaconda commands to be excecuted to create and run Prodigy server.

---
+++ run/run_win.bat
@@ -0,0 +1,5 @@
+@echo on
+cd "C:\Dev Projects\decide-madrid-2019-labels\prodigy-scripts"
+call conda activate base
+call conda activate prodigy_env
+call python -m prodigy rel.manual intra_andres blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/50.jsonl" --label CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM --wrap
