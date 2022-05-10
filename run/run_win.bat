2b3e16396c9fb3a9330c210eb446d44e82614da3
OscarGB <oscar.gomez.borz@gmail.com>
2022-05-10 15:14:54 +0200

    windows script

--- run/run_win.bat
+++ run/run_win.bat
@@ -2,4 +2,4 @@
 cd "C:\Dev Projects\decide-madrid-2019-labels\prodigy-scripts"
 call conda activate base
 call conda activate prodigy_env
-call python -m prodigy rel.manual intra_andres blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/50.jsonl" --label CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM --wrap
+call python -m prodigy rel.manual intra_andres blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/50.jsonl" --label LINKS,CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM,LINKER --wrap
