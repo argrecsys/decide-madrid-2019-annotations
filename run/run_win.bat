c59b85851cf18542718621d2716b50e7a2aaecfb
Andres Segura-Tinoco <seguraandres7@gmail.com>
2022-05-31 00:02:21 +0200

    Update Annotations
    The Ivan's annotations have been added.

--- run/run_win.bat
+++ run/run_win.bat
@@ -1,5 +1,6 @@
 @echo on
 cd "C:\Dev Projects\decide-madrid-2019-labels\prodigy-scripts"
-call conda activate base
-call conda activate prodigy_env
-call python -m prodigy rel.manual intra_andres blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/50.jsonl" --label LINKS,CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM,LINKER --wrap
+conda activate prodigy_env
+python -m prodigy rel.manual intra_ivan blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/417.jsonl" --label LINKS,CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM,LINKER --wrap
+python -m prodigy db-out intra_ivan "C:/Dev Projects/decide-madrid-2019-labels/results/andres-05302022-232930"
+start chrome http://localhost:8080/
