3b18da4a210ba4d3c5bf7d56d1b0bc849d7d9b29
Andres Segura-Tinoco <seguraandres7@gmail.com>
2022-06-02 17:45:45 +0200

    Update Annotations
    The annotations to proposal 417 have been added.

--- run/run_win.bat
+++ run/run_win.bat
@@ -2,5 +2,5 @@
 cd "C:\Dev Projects\decide-madrid-2019-labels\prodigy-scripts"
 conda activate prodigy_env
 python -m prodigy rel.manual intra_ivan blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/417.jsonl" --label LINKS,CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM,LINKER --wrap
-python -m prodigy db-out intra_ivan "C:/Dev Projects/decide-madrid-2019-labels/results/andres-05302022-232930"
+python -m prodigy db-out intra_ivan "C:/Dev Projects/decide-madrid-2019-labels/results/andres-06022022-180115"
 start chrome http://localhost:8080/
