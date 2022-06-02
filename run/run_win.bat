a642915c33c7b100b5bbef120b4a7315a0c2dea3
Andres Segura-Tinoco <seguraandres7@gmail.com>
2022-06-02 23:39:36 +0200

    Update Annotation
    The annotations to proposal 507 have been added.

--- run/run_win.bat
+++ run/run_win.bat
@@ -1,6 +1,6 @@
 @echo on
 cd "C:\Dev Projects\decide-madrid-2019-labels\prodigy-scripts"
 conda activate prodigy_env
-python -m prodigy rel.manual intra_ivan blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/494.jsonl" --label LINKS,CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM,LINKER --wrap
-python -m prodigy db-out intra_ivan "C:/Dev Projects/decide-madrid-2019-labels/results/andres-06022022-183045"
+python -m prodigy rel.manual intra_ivan blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/507.jsonl" --label LINKS,CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM,LINKER --wrap
+python -m prodigy db-out intra_ivan "C:/Dev Projects/decide-madrid-2019-labels/results/andres-06022022-233651"
 start chrome http://localhost:8080/
