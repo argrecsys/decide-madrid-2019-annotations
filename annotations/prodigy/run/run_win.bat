7d225e7b7d8142d23d302c0fd37b24e3bb68b71a
Andres Segura-Tinoco <seguraandres7@gmail.com>
2023-05-12 16:28:43 +0200

    Update Annotations folders
    The folder structure of the annotations made with both tools (argael and prodigy) has been reorganized.

--- annotations/prodigy/run/run_win.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-cd "C:\Dev Projects\decide-madrid-2019-labels\prodigy-scripts"
-conda activate prodigy_env
-python -m prodigy rel.manual intra_ivan blank:es "C:/Dev Projects/decide-madrid-2019-labels/data/7.jsonl" --label LINKS,CONDITION,REASON,CONCLUSION,EXEMPLIFICATION,RESTATEMENT,SUMMARY,EXPLANATION,GOAL,RESULT,ALTERNATIVE,COMPARISON,CONCESSION,OPPOSITION,ADDITION,PRECISION,SIMILARITY,SUPPORT,ATTACK --span-label PREMISE,CLAIM,MAJOR_CLAIM,LINKER --wrap
-python -m prodigy db-out intra_ivan "C:/Dev Projects/decide-madrid-2019-labels/results/ivan-06062022-235531"
-start chrome http://localhost:8080/
